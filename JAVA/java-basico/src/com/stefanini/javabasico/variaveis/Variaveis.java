a9870851fd68dba7615e596e4ac44952b45d6274
GuilhermeAlex <guilhermealex01@gmail.com>
2021-04-13 15:23:08 -0300

    Tipos de Variaveis

---
+++ JAVA/java-basico/src/com/stefanini/javabasico/variaveis/Variaveis.java
@@ -0,0 +1,26 @@
+package com.stefanini.javabasico.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		//Exercicio 2 Variaveis 
+		
+		long numLong = 2147483649L;
+		byte numeroByte = 120;
+		short numShort = 150;
+		String text = "Bem vindos Devs!";
+		String numeroX = "0x2202";
+		double numDouble = 99999.987;
+		
+		
+		System.out.println(numLong);
+		System.out.println(numeroByte);
+		System.out.println(numShort);
+		System.out.println(text);
+		System.out.println(numeroX);
+		System.out.println(numDouble);
+		
+
+	}
+
+}
