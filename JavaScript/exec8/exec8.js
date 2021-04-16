338900225ff7c3b5b174e7f7be0a7918ede5af4d
GuilhermeAlex <guilhermealex01@gmail.com>
2021-04-16 19:18:00 -0300

    implementando função

---
+++ JavaScript/exec8/exec8.js
@@ -0,0 +1,8 @@
+var soma = function (x,y){
+    return x + y;  
+}
+
+var valor1 = Number(prompt("Digite o primeiro valor: "));
+var valor2 = Number(prompt("Digite o segundo valor: "));
+
+alert("Resultado: "+ soma(valor1,valor2));
