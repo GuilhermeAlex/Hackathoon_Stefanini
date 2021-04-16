d2c56fdccfadbf79cc4bbb380d9325ff666974e3
GuilhermeAlex <guilhermealex01@gmail.com>
2021-04-16 17:49:49 -0300

    calculadora com limitador

---
+++ JavaScript/exec5/exec5.js
@@ -0,0 +1,8 @@
+var x = prompt("Insira um numero para x: ");
+var y = prompt("Insira um numero para y: ");
+
+var soma = (x+y);
+//processamento
+if(soma > 10){
+alert("Essa calculadora é limitada em resultados até 10...")
+}
